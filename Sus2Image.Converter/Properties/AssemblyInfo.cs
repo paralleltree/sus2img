bd598228b24e7b6abedf7133a7c413a8bac53e1e
Paralleltree <paralleltree@outlook.com>
2018-11-10 17:25:44 +0900

    Add Sus2Image.Converter project

---
+++ Sus2Image.Converter/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sus2Image.Converter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sus2Image.Converter")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("22045cd0-d9bd-45fc-a282-820f686b10ac")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
