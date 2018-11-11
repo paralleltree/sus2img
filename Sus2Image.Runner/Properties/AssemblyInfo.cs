91eb9f46a5a511909a840d85fbea3c7a7f08cac1
Paralleltree <paralleltree@outlook.com>
2018-11-11 21:10:56 +0900

    Add Sus2Image.Runner project

---
+++ Sus2Image.Runner/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sus2Image.Runner")]
+[assembly: AssemblyDescription("Sus2Image Console Application")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyProduct("Sus2Image.Runner")]
+[assembly: AssemblyCopyright("Copyright (C) 2018 Paralleltree")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("51b4474c-ec38-4aad-bef5-586907e8a315")]
+[assembly: AssemblyVersion("1.0.0.0")]
