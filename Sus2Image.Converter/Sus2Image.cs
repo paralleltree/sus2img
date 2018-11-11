06fe309126f125a09b1e795afe3763a962734d11
Paralleltree <paralleltree@outlook.com>
2018-11-12 00:01:00 +0900

    画像への変換実装の巻

--- Sus2Image.Converter/Sus2Image.cs
+++ Sus2Image.Converter/Sus2Image.cs
@@ -7,23 +7,313 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ConcurrentPriorityQueue;
+using Ched.Core.Notes;
 using Ched.Drawing;
 
 namespace Sus2Image.Converter
 {
     public class Sus2Image
     {
+        protected SusScoreData Score { get; set; }
+        protected int TicksPerBeat => Score.TicksPerBeat;
         public MeasurementProfile MeasurementProfile { get; set; }
         public ColorProfile NoteColorProfile { get; set; }
         public BackgroundColorProfile BackgroundColorProfile { get; set; }
 
         public Sus2Image(StreamReader reader)
         {
+            Score = new SusParser().Parse(reader);
         }
 
         public Image Convert()
         {
-            throw new NotImplementedException();
+            var sigs = Score.TimeSignatures.OrderBy(p => p.Key).ToList();
+            int lanesCount = Ched.Core.Constants.LanesCount;
+            int columnTick = MeasurementProfile.CalcColumnTickFromTicksPerBeat(Score.TicksPerBeat);
+            int paddingTick = MeasurementProfile.CalcPaddingTickFromTicksPerBeat(Score.TicksPerBeat);
+            float laneWidth = MeasurementProfile.UnitLaneWidth + MeasurementProfile.BorderThickness;
+            float wholeLaneWidth = laneWidth * lanesCount;
+            float columnHeight = columnTick * MeasurementProfile.UnitBeatHeight / Score.TicksPerBeat;
+            float paddingHeight = paddingTick * MeasurementProfile.UnitBeatHeight / Score.TicksPerBeat;
+            int headTick = 0;
+
+            var bmp = new Bitmap((int)(MeasurementProfile.PaddingWidth * 2 + wholeLaneWidth) * (int)Math.Ceiling((double)Score.GetLastTick() / columnTick), (int)(paddingHeight * 2 + columnHeight));
+            var longEndsPos = new HashSet<NotePosition>(Score.LongNotes['2'].Concat(Score.LongNotes['3']).Select(p => p.Last().Item2));
+            var unUsedShortNoteQueues = Score.ShortNotes.ToDictionary(p => p.Key, p =>
+            {
+                var q = new ConcurrentPriorityQueue<Tuple<char, NotePosition>, int>();
+                foreach (var item in p.Value) q.Enqueue(item, -item.Item2.Tick);
+                return q;
+            });
+            var usingShortNoteQueues = Score.ShortNotes.ToDictionary(p => p.Key, p => new ConcurrentPriorityQueue<Tuple<char, NotePosition>, int>());
+            var unUsedLongNoteQueues = Score.LongNotes.ToDictionary(p => p.Key, p =>
+            {
+                var q = new ConcurrentPriorityQueue<List<Tuple<char, NotePosition>>, int>();
+                foreach (var item in p.Value) q.Enqueue(item, -item[0].Item2.Tick);
+                return q;
+            });
+            var usingLongNoteQueues = Score.LongNotes.ToDictionary(p => p.Key, p => new ConcurrentPriorityQueue<List<Tuple<char, NotePosition>>, int>());
+            var emptyLong = Enumerable.Empty<List<Tuple<char, NotePosition>>>();
+
+            Func<Tuple<char, NotePosition>, bool> visibleStep = p => p.Item1 == '1' || p.Item1 == '2' || p.Item1 == '3';
+            Action<Graphics> moveToOrigin = g => g.TranslateTransform(MeasurementProfile.PaddingWidth, paddingHeight + columnHeight - 1, MatrixOrder.Append);
+            Action<Graphics, int> moveToColumn = (g, columnsCount) => g.TranslateTransform((MeasurementProfile.PaddingWidth * 2 + wholeLaneWidth) * columnsCount, 0, MatrixOrder.Append);
+            Action<Graphics, int> moveToHead = (g, head) => g.TranslateTransform(0, head * MeasurementProfile.UnitBeatHeight / TicksPerBeat, MatrixOrder.Append);
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                int columnsCount = 0;
+                SizeF strSize = g.MeasureString("000", MeasurementProfile.Font);
+                var dc = new DrawingContext(g, NoteColorProfile);
+                g.Clear(BackgroundColorProfile.BackgroundColor);
+
+                while (unUsedShortNoteQueues.Any(p => p.Value.Count > 0) || usingShortNoteQueues.Any(p => p.Value.Count > 0) || usingLongNoteQueues.Any(p => p.Value.Count > 0) || unUsedLongNoteQueues.Any(p => p.Value.Count > 0))
+                {
+                    int tailTick = headTick + columnTick;
+
+                    // 範囲外に出たノーツの更新
+                    foreach (var type in usingShortNoteQueues)
+                    {
+                        while (type.Value.Count > 0 && type.Value.Peek().Item2.Tick < headTick - paddingTick) type.Value.Dequeue();
+                    }
+
+                    foreach (var type in usingLongNoteQueues)
+                    {
+                        while (type.Value.Count > 0 && type.Value.Peek()[type.Value.Peek().Count - 1].Item2.Tick < headTick - paddingTick) type.Value.Dequeue();
+                    }
+
+                    // 範囲内に入るノーツの更新
+                    foreach (var type in unUsedShortNoteQueues)
+                    {
+                        while (type.Value.Count > 0 && type.Value.Peek().Item2.Tick < tailTick + paddingTick)
+                        {
+                            var item = type.Value.Dequeue();
+                            usingShortNoteQueues[type.Key].Enqueue(item, -item.Item2.Tick);
+                        }
+                    }
+
+                    foreach (var type in unUsedLongNoteQueues)
+                    {
+                        while (type.Value.Count > 0 && type.Value.Peek()[0].Item2.Tick < tailTick + paddingTick)
+                        {
+                            var item = type.Value.Dequeue();
+                            usingLongNoteQueues[type.Key].Enqueue(item, -item[item.Count - 1].Item2.Tick);
+                        }
+                    }
+
+                    g.ResetTransform();
+                    g.ScaleTransform(1, -1);
+                    moveToOrigin(g);
+                    moveToHead(g, headTick);
+                    moveToColumn(g, columnsCount);
+
+                    // レーン分割線描画
+                    using (var pen = new Pen(BackgroundColorProfile.LaneBorderColor, MeasurementProfile.BorderThickness))
+                    {
+                        for (int i = 0; i <= lanesCount; i++)
+                        {
+                            if (i % 2 != 0) continue;
+                            float x = i * laneWidth;
+                            g.DrawLine(pen, x, GetYPositionFromTick(headTick - paddingTick), x, GetYPositionFromTick(tailTick + paddingTick));
+                        }
+                    }
+
+                    // 時間ガイドの描画
+                    // そのイベントが含まれる小節(ただし[小節開始Tick, 小節開始Tick + 小節Tick)の範囲)からその拍子を適用
+
+                    using (var beatPen = new Pen(BackgroundColorProfile.BeatLineColor, MeasurementProfile.BorderThickness))
+                    using (var barPen = new Pen(BackgroundColorProfile.BarLineColor, MeasurementProfile.BorderThickness))
+                    {
+                        // 最初の拍子
+                        int firstBarTick = (int)(TicksPerBeat * sigs[0].Value);
+                        int firstBeatTick = Math.Min(TicksPerBeat, firstBarTick);
+                        for (int i = headTick / firstBeatTick; sigs.Count < 2 || i * firstBeatTick < sigs[1].Key / firstBarTick * firstBarTick; i++)
+                        {
+                            int tick = i * firstBeatTick;
+                            float y = GetYPositionFromTick(tick);
+                            g.DrawLine(tick % firstBarTick == 0 ? barPen : beatPen, 0, y, wholeLaneWidth, y);
+                            if (tick > tailTick) break;
+                        }
+
+                        // その後の拍子
+                        int pos = 0;
+                        for (int j = 1; j < sigs.Count; j++)
+                        {
+                            int prevBarTick = (int)(TicksPerBeat * sigs[j - 1].Value);
+                            int currentBarTick = (int)(TicksPerBeat * sigs[j].Value);
+                            int currentBeatTick = Math.Min(TicksPerBeat, currentBarTick);
+                            pos += (sigs[j].Key - pos) / prevBarTick * prevBarTick;
+                            if (pos > tailTick) break;
+                            for (int i = headTick - pos < 0 ? 0 : (headTick - pos) / currentBeatTick; pos + i * (int)(TicksPerBeat / sigs[j].Value) < tailTick; i++)
+                            {
+                                if (j < sigs.Count - 1 && i * currentBeatTick >= (sigs[j + 1].Key - pos) / currentBarTick * currentBarTick) break;
+                                int tick = pos + i * currentBeatTick;
+                                float y = GetYPositionFromTick(tick);
+                                g.DrawLine((tick - pos) % currentBarTick == 0 ? barPen : beatPen, 0, y, wholeLaneWidth, y);
+                            }
+                        }
+                    }
+
+
+                    // ノーツ描画
+                    foreach (var hold in usingLongNoteQueues.ContainsKey('2') ? usingLongNoteQueues['2'] : emptyLong)
+                    {
+                        dc.DrawHoldBackground(new RectangleF(
+                            (MeasurementProfile.UnitLaneWidth + MeasurementProfile.BorderThickness) * hold[0].Item2.LaneIndex + MeasurementProfile.BorderThickness,
+                            GetYPositionFromTick(hold[0].Item2.Tick),
+                            (MeasurementProfile.UnitLaneWidth + MeasurementProfile.BorderThickness) * hold[0].Item2.Width - MeasurementProfile.BorderThickness,
+                            GetYPositionFromTick(hold[1].Item2.Tick - hold[0].Item2.Tick)
+                            ));
+                    }
+
+                    foreach (var slide in usingLongNoteQueues.ContainsKey('3') ? usingLongNoteQueues['3'] : emptyLong)
+                    {
+                        var visibleSteps = slide.Where(visibleStep).ToList();
+                        for (int i = 0; i < slide.Count - 1; i++)
+                        {
+                            dc.DrawSlideBackground(
+                                laneWidth * slide[i].Item2.Width - MeasurementProfile.BorderThickness,
+                                laneWidth * slide[i + 1].Item2.Width - MeasurementProfile.BorderThickness,
+                                laneWidth * slide[i].Item2.LaneIndex,
+                                GetYPositionFromTick(slide[i].Item2.Tick),
+                                laneWidth * slide[i + 1].Item2.LaneIndex,
+                                GetYPositionFromTick(slide[i + 1].Item2.Tick) + 0.4f,
+                                GetYPositionFromTick(visibleSteps.Last(p => p.Item2.Tick <= slide[i].Item2.Tick).Item2.Tick),
+                                GetYPositionFromTick(visibleSteps.First(p => p.Item2.Tick >= slide[i + 1].Item2.Tick).Item2.Tick),
+                                MeasurementProfile.ShortNoteHeight);
+                        }
+                    }
+
+                    foreach (var airAction in usingLongNoteQueues.ContainsKey('4') ? usingLongNoteQueues['4'] : emptyLong)
+                    {
+                        dc.DrawAirHoldLine(
+                            laneWidth * (airAction[0].Item2.LaneIndex + airAction[0].Item2.Width / 2f),
+                            GetYPositionFromTick(airAction[0].Item2.Tick),
+                            GetYPositionFromTick(airAction[airAction.Count - 1].Item2.Tick),
+                            MeasurementProfile.ShortNoteHeight);
+                    }
+
+                    foreach (var hold in usingLongNoteQueues.ContainsKey('2') ? usingLongNoteQueues['2'] : emptyLong)
+                    {
+                        dc.DrawHoldBegin(GetRectFromNotePosition(hold[0].Item2));
+                        dc.DrawHoldEnd(GetRectFromNotePosition(hold[hold.Count - 1].Item2));
+                    }
+
+                    foreach (var slide in usingLongNoteQueues.ContainsKey('3') ? usingLongNoteQueues['3'] : emptyLong)
+                    {
+                        dc.DrawSlideBegin(GetRectFromNotePosition(slide[0].Item2));
+                        foreach (var item in slide.Where(visibleStep).Skip(1))
+                        {
+                            if (item.Item2.Tick < headTick - paddingTick) continue;
+                            if (item.Item2.Tick > tailTick + paddingTick) break;
+                            dc.DrawSlideStep(GetRectFromNotePosition(item.Item2));
+                        }
+                    }
+
+                    // ロング終点AIR
+                    var airs = usingShortNoteQueues.ContainsKey('5') ? usingShortNoteQueues['5'] : Enumerable.Empty<Tuple<char, NotePosition>>();
+                    foreach (var air in airs)
+                    {
+                        if (!longEndsPos.Contains(air.Item2)) continue;
+                        dc.DrawAirStep(GetRectFromNotePosition(air.Item2));
+                    }
+
+                    var shortNotesDic = usingShortNoteQueues['1'].GroupBy(p => p.Item1).ToDictionary(p => p.Key, p => p.Select(q => q.Item2));
+                    Action<char, Action<NotePosition>> drawNotes = (key, drawer) =>
+                    {
+                        if (!shortNotesDic.ContainsKey(key)) return;
+                        foreach (var item in shortNotesDic[key]) drawer(item);
+                    };
+
+                    drawNotes('1', item => dc.DrawTap(GetRectFromNotePosition(item)));
+                    drawNotes('2', item => dc.DrawExTap(GetRectFromNotePosition(item)));
+                    drawNotes('5', item => dc.DrawExTap(GetRectFromNotePosition(item)));
+                    drawNotes('6', item => dc.DrawExTap(GetRectFromNotePosition(item)));
+                    drawNotes('3', item => dc.DrawFlick(GetRectFromNotePosition(item)));
+                    drawNotes('4', item => dc.DrawDamage(GetRectFromNotePosition(item)));
+
+                    foreach (var airAction in usingLongNoteQueues.ContainsKey('4') ? usingLongNoteQueues['4'] : emptyLong)
+                    {
+                        foreach (var item in airAction.Skip(1))
+                        {
+                            if (item.Item2.Tick < headTick - paddingTick) continue;
+                            if (item.Item2.Tick > tailTick + paddingTick) break;
+                            dc.DrawAirAction(GetRectFromNotePosition(item.Item2).Expand(-MeasurementProfile.ShortNoteHeight * 0.28f));
+                        }
+                    }
+
+                    foreach (var air in airs)
+                    {
+                        var vd = air.Item1 == '2' || air.Item1 == '5' || air.Item1 == '6' ? VerticalAirDirection.Down : VerticalAirDirection.Up;
+                        var hd = air.Item1 == '1' || air.Item1 == '2' || air.Item1 == '7' ? HorizontalAirDirection.Center :
+                            (air.Item1 == '3' || air.Item1 == '5' || air.Item1 == '8' ? HorizontalAirDirection.Left : HorizontalAirDirection.Right);
+                        dc.DrawAir(GetRectFromNotePosition(air.Item2), vd, hd);
+                    }
+
+                    g.ResetTransform();
+                    moveToOrigin(g);
+                    moveToHead(g, headTick);
+                    moveToColumn(g, columnsCount);
+
+                    // 小節番号
+                    using (var brush = new SolidBrush(BackgroundColorProfile.BarIndexColor))
+                    {
+                        int pos = 0;
+                        int barCount = 0;
+                        for (int j = 0; j < sigs.Count; j++)
+                        {
+                            int currentBarTick = (int)(TicksPerBeat * sigs[j].Value);
+                            for (int i = 0; pos + i * currentBarTick < tailTick; i++)
+                            {
+                                if (j < sigs.Count - 1 && i * currentBarTick >= (sigs[j + 1].Key - pos) / currentBarTick * currentBarTick) break;
+
+                                int tick = pos + i * currentBarTick;
+                                barCount++;
+                                if (tick < headTick) continue;
+                                var point = new PointF(-strSize.Width, -GetYPositionFromTick(tick) - strSize.Height);
+                                g.DrawString(string.Format("{0:000}", barCount), MeasurementProfile.Font, brush, point);
+                            }
+
+                            if (j < sigs.Count - 1)
+                                pos += (sigs[j + 1].Key - pos) / currentBarTick * currentBarTick;
+                        }
+                    }
+
+                    float rightBaseX = wholeLaneWidth + strSize.Width / 3;
+
+                    // BPM
+                    using (var brush = new SolidBrush(BackgroundColorProfile.BpmColor))
+                    {
+                        foreach (var item in Score.BpmDefinitions.Where(p => p.Key >= headTick && p.Key < tailTick))
+                        {
+                            var point = new PointF(rightBaseX, -GetYPositionFromTick(item.Key) - strSize.Height);
+                            g.DrawString(string.Format("{0:000.#}", item.Value), MeasurementProfile.Font, brush, point);
+                        }
+                    }
+
+                    // 次の列に移動
+                    headTick += columnTick;
+                    columnsCount++;
+                }
+            }
+
+            return bmp;
+        }
+
+        protected RectangleF GetRectFromNotePosition(NotePosition pos)
+        {
+            return new RectangleF(
+                (MeasurementProfile.UnitLaneWidth + MeasurementProfile.BorderThickness) * pos.LaneIndex + MeasurementProfile.BorderThickness,
+                GetYPositionFromTick(pos.Tick) - MeasurementProfile.ShortNoteHeight / 2,
+                (MeasurementProfile.UnitLaneWidth + MeasurementProfile.BorderThickness) * pos.Width - MeasurementProfile.BorderThickness,
+                MeasurementProfile.ShortNoteHeight
+                );
+        }
+
+        protected float GetYPositionFromTick(int tick)
+        {
+            return tick * MeasurementProfile.UnitBeatHeight / TicksPerBeat;
         }
     }
 
---
+++ Sus2Image.Converter/SusScoreData.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Ched.Core.Notes;
+
+namespace Sus2Image.Converter
+{
+    public class SusScoreData
+    {
+        public int TicksPerBeat { get; set; } = 192;
+        public Dictionary<int, decimal> BpmDefinitions { get; set; } = new Dictionary<int, decimal>() { { 0, 120.0m } };
+        public Dictionary<int, double> TimeSignatures { get; set; } = new Dictionary<int, double>() { { 0, 4.0 } };
+
+        public Dictionary<char, List<Tuple<char, NotePosition>>> ShortNotes { get; set; } = new Dictionary<char, List<Tuple<char, NotePosition>>>();
+        public Dictionary<char, List<List<Tuple<char, NotePosition>>>> LongNotes { get; set; }
+
+        public int GetLastTick()
+        {
+            return Math.Max(ShortNotes.Max(p => p.Value.Max(q => q.Item2.Tick)), LongNotes.Max(p => p.Value.Max(r => r.Max(s => s.Item2.Tick))));
+        }
+    }
+
+    public class SusParser
+    {
+        private Regex SusCommandPattern { get; } = new Regex(@"#(?<name>[A-Z]+)\s+\""?(?<value>[^\""]*)", RegexOptions.IgnoreCase);
+        private Regex SusNotePattern { get; } = new Regex(@"#(?<barIndex>\d{3})(?<type>[1-5])(?<laneIndex>[0-9a-f])(?<longKey>[0-9a-z])?:(?<data>([0-9a-g]{2}|\s)+)", RegexOptions.IgnoreCase);
+        private Regex BpmDefinitionPattern { get; } = new Regex(@"#BPM(?<key>[0-9a-f]{2}):\s*(?<value>[0-9.]+)", RegexOptions.IgnoreCase);
+        private Regex BpmCommandPattern { get; } = new Regex(@"#(?<barIndex>\d{3})08:\s*(?<data>[0-9a-f\s]+)", RegexOptions.IgnoreCase);
+        private Regex TimeSignatureCommandPattern { get; } = new Regex(@"(?<barIndex>\d{3})02:\s*(?<value>[0-9.]+)");
+
+        private int TicksPerBeat = 192;
+        private Dictionary<string, decimal> BpmDefinitions = new Dictionary<string, decimal>();
+        private SusScoreData Score = new SusScoreData();
+
+        public SusScoreData Parse(StreamReader reader)
+        {
+            var sigs = new Dictionary<int, double>();
+
+            var shortNotesData = new List<Match>();
+            var longNotesData = new List<Match>();
+            var bpmData = new List<Match>();
+            Func<Match, Action<Match>, bool> matchAction = (m, act) =>
+            {
+                if (m.Success) act(m);
+                return m.Success;
+            };
+
+            while (reader.Peek() >= 0)
+            {
+                string line = reader.ReadLine();
+
+                if (matchAction(SusCommandPattern.Match(line), m => ProcessCommand(m.Groups["name"].Value, m.Groups["value"].Value))) continue;
+
+                if (matchAction(BpmDefinitionPattern.Match(line), m => BpmDefinitions.Add(m.Groups["key"].Value, decimal.Parse(m.Groups["value"].Value)))) continue;
+
+                if (matchAction(BpmCommandPattern.Match(line), m => bpmData.Add(m))) continue;
+
+                if (matchAction(SusNotePattern.Match(line), m => (m.Groups["longKey"].Success ? longNotesData : shortNotesData).Add(m))) continue;
+
+                if (matchAction(TimeSignatureCommandPattern.Match(line), m => sigs.Add(int.Parse(m.Groups["barIndex"].Value), double.Parse(m.Groups["value"].Value)))) continue;
+            }
+
+            var barIndexCalculator = new BarIndexCalculator(TicksPerBeat, sigs);
+
+            var bpmDic = bpmData.SelectMany(p => SplitData(barIndexCalculator, int.Parse(p.Groups["barIndex"].Value), p.Groups["data"].Value))
+                .Where(p => p.Item2 != "00")
+                .ToDictionary(p => p.Item1, p => BpmDefinitions[p.Item2]);
+
+            // データ種別と位置
+            var shortNotes = shortNotesData.GroupBy(p => p.Groups["type"].Value[0]).ToDictionary(p => p.Key, p => p.SelectMany(q =>
+            {
+                return SplitData(barIndexCalculator, int.Parse(q.Groups["barIndex"].Value), q.Groups["data"].Value)
+                    .Where(r => r.Item2 != "00")
+                    .Select(r => Tuple.Create(r.Item2[0], new NotePosition() { Tick = r.Item1, LaneIndex = ConvertHex(q.Groups["laneIndex"].Value[0]), Width = ConvertHex(r.Item2[1]) }));
+            }).ToList());
+
+            // ロング種別 -> ロングノーツリスト -> 構成点リスト
+            var longNotes = longNotesData.GroupBy(p => p.Groups["type"].Value[0]).ToDictionary(p => p.Key, p =>
+            {
+                return p.GroupBy(q => q.Groups["longKey"].Value.ToUpper()).Select(q => q.SelectMany(r =>
+                {
+                    return SplitData(barIndexCalculator, int.Parse(r.Groups["barIndex"].Value), r.Groups["data"].Value)
+                        .Where(s => s.Item2 != "00")
+                        .Select(s => Tuple.Create(s.Item2[0], new NotePosition() { Tick = s.Item1, LaneIndex = ConvertHex(r.Groups["laneIndex"].Value[0]), Width = ConvertHex(s.Item2[1]) }));
+                }))
+                .SelectMany(q => FlatSplitLongNotes(q, '1', '2'))
+                .ToList();
+            });
+
+
+            return new SusScoreData()
+            {
+                TicksPerBeat = this.TicksPerBeat,
+                BpmDefinitions = bpmDic,
+                TimeSignatures = sigs.ToDictionary(p => barIndexCalculator.GetTickFromBarIndex(p.Key), p => p.Value),
+                ShortNotes = shortNotes,
+                LongNotes = longNotes
+            };
+        }
+
+        protected void ProcessCommand(string name, string value)
+        {
+            switch (name.ToUpper())
+            {
+                case "REQUEST":
+                    var tpb = Regex.Match(value, @"(?<=ticks_per_beat )\d+");
+                    if (tpb.Success) TicksPerBeat = int.Parse(tpb.Value);
+                    break;
+            }
+        }
+
+        // 同一識別子を持つロングノーツのリストをロングノーツ単体に分解します
+        protected IEnumerable<List<Tuple<char, NotePosition>>> FlatSplitLongNotes(IEnumerable<Tuple<char, NotePosition>> data, char beginChar, char endChar)
+        {
+            var enumerator = data.OrderBy(p => p.Item2.Tick).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current.Item1 != beginChar) throw new InvalidOperationException();
+                var longNote = new List<Tuple<char, NotePosition>>() { enumerator.Current };
+                while (enumerator.MoveNext())
+                {
+                    longNote.Add(enumerator.Current);
+                    if (enumerator.Current.Item1 == endChar) break;
+                }
+
+                yield return longNote;
+            }
+        }
+
+        protected IEnumerable<Tuple<int, string>> SplitData(BarIndexCalculator c, int barIndex, string data)
+        {
+            data = Regex.Replace(data, @"\s+", "");
+            int headTick = c.GetTickFromBarIndex(barIndex);
+            int barTick = (int)(c.GetBarBeatsFromBarIndex(barIndex) * TicksPerBeat);
+            var list = Enumerable.Range(0, data.Length / 2).Select(p => data.Substring(p * 2, 2)).ToList();
+            return list.Select((p, i) => Tuple.Create(headTick + (int)(barTick * ((double)i / list.Count)), p));
+        }
+
+        protected int ConvertHex(char c)
+        {
+            return c == 'g' ? 16 : Convert.ToInt32(c.ToString(), 16);
+        }
+    }
+
+    public class BarIndexCalculator
+    {
+        private int TicksPerBeat { get; } = 192;
+        private List<Tuple<int, int, double>> Definitions { get; } = new List<Tuple<int, int, double>>(); // barIndex, headTick, barBeats
+
+        public BarIndexCalculator(int ticksPerBeat, Dictionary<int, double> sigs) // barIndex, barBeats
+        {
+            TicksPerBeat = ticksPerBeat;
+            var ordered = sigs.OrderBy(p => p.Key).ToList();
+            int pos = 0;
+
+            for (int i = 0; i < ordered.Count; i++)
+            {
+                Definitions.Add(Tuple.Create(ordered[i].Key, pos, ordered[i].Value));
+                if (i < ordered.Count - 1)
+                    pos += (int)((ordered[i + 1].Key - ordered[i].Key) * ticksPerBeat * ordered[i].Value);
+            }
+        }
+
+        public int GetTickFromBarIndex(int barIndex)
+        {
+            var def = GetDefinitionFromBarIndex(barIndex);
+            return def.Item2 + (int)((barIndex - def.Item1) * TicksPerBeat * def.Item3);
+
+        }
+
+        public double GetBarBeatsFromBarIndex(int barIndex)
+        {
+            return GetDefinitionFromBarIndex(barIndex).Item3;
+        }
+
+        protected Tuple<int, int, double> GetDefinitionFromBarIndex(int barIndex)
+        {
+            int i = 0;
+            while (i < Definitions.Count && barIndex >= Definitions[i].Item1) i++;
+            return Definitions[i - 1];
+        }
+    }
+
+    public struct NotePosition
+    {
+        public int Tick { get; set; }
+        public int LaneIndex { get; set; }
+        public int Width { get; set; }
+    }
+}
