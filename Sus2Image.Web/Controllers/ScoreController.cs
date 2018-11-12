d26d6615bd4a459428c9fdbe3713a1e8a24da882
Paralleltree <paralleltree@outlook.com>
2018-11-12 15:05:44 +0900

    Add Sus2Image.Web project

---
+++ Sus2Image.Web/Controllers/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sus2Image.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreController : ControllerBase
+    {
+        [HttpPost]
+        [Route("convert")]
+        public IActionResult ConvertSusToImage()
+        {
+            return StatusCode(501);
+        }
+    }
+}
