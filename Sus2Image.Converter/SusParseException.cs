c3f470e470aa041b923df66efea8195ea402f13c
Paralleltree <paralleltree@outlook.com>
2018-11-14 13:18:04 +0900

    SusParseException追加

---
+++ Sus2Image.Converter/SusParseException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sus2Image.Converter
+{
+    [Serializable]
+    public class SusParseException : Exception
+    {
+        public SusParseException()
+        {
+        }
+
+        public SusParseException(string message) : base(message)
+        {
+        }
+
+        public SusParseException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected SusParseException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
