ce4eb6627866ac23a466012c24f203f632c4ca4e
Paralleltree <paralleltree@outlook.com>
2019-02-10 16:15:23 +0900

    インポートプラグインをAIR対応

--- SusImportPlugin/SusImportPlugin.cs
+++ SusImportPlugin/SusImportPlugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Ched.Core;
@@ -16,7 +17,7 @@ namespace Ched.Plugins
     {
         public string FileFilter => "susファイル(*.sus)|*.sus";
 
-        public string DisplayName => "Seaurchin Score File(AIR抜き)";
+        public string DisplayName => "Seaurchin Score File";
 
         public ScoreBook Import(TextReader reader)
         {
@@ -100,6 +101,59 @@ namespace Ched.Plugins
                     slide.StepNotes.Add(stepTap);
                 }
                 res.Notes.Slides.Add(slide);
+            }
+
+            var airables = res.Notes.GetShortNotes().Cast<IAirable>()
+                .Concat(res.Notes.Holds.Select(p => p.EndNote))
+                .Concat(res.Notes.Slides.Select(p => p.StepNotes.OrderByDescending(q => q.Tick).First()));
+            var airablesDic = airables.Select(p => new { Note = p, Position = new NotePosition() { Tick = p.Tick, LaneIndex = p.LaneIndex, Width = p.Width } })
+                .GroupBy(p => p.Position)
+                .ToDictionary(p => p.Key, p => p.Select(q => q.Note).ToList());
+
+            var usedAirs = new HashSet<IAirable>();
+            foreach (var item in raw.ShortNotes['5'])
+            {
+                if (!airablesDic.ContainsKey(item.Item2)) continue;
+                foreach (var airable in airablesDic[item.Item2])
+                {
+                    if (usedAirs.Contains(airable)) continue;
+                    var air = new Air(airable) { VerticalDirection = Regex.IsMatch(item.Item1.ToString(), "1|3|4") ? VerticalAirDirection.Up : VerticalAirDirection.Down };
+                    switch (item.Item1)
+                    {
+                        case '1':
+                        case '2':
+                            air.HorizontalDirection = HorizontalAirDirection.Center;
+                            break;
+
+                        case '3':
+                        case '5':
+                            air.HorizontalDirection = HorizontalAirDirection.Left;
+                            break;
+
+                        case '4':
+                        case '6':
+                            air.HorizontalDirection = HorizontalAirDirection.Right;
+                            break;
+                    }
+                    res.Notes.Airs.Add(air);
+                    usedAirs.Add(airable);
+                    break;
+                }
+            }
+
+            var usedAirActions = new HashSet<IAirable>();
+            foreach (var item in raw.LongNotes['4'])
+            {
+                if (!airablesDic.ContainsKey(item[0].Item2)) continue;
+                foreach (var airable in airablesDic[item[0].Item2])
+                {
+                    if (usedAirActions.Contains(airable)) continue;
+                    var airAction = new AirAction(airable);
+                    airAction.ActionNotes.AddRange(item.Skip(1).Select(p => new AirAction.ActionNote(airAction) { Offset = p.Item2.Tick - item[0].Item2.Tick }));
+                    res.Notes.AirActions.Add(airAction);
+                    usedAirActions.Add(airable);
+                    break;
+                }
             }
 
             return res;
